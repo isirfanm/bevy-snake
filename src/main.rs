3a570a9a571b51bd5d7a10fded67990f6d01cf0c
Mohamad Irfan Islami <positron06max@gmail.com>
2025-01-14 21:21:26 +0700

    snake head

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,31 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d::default());
 }
 
+#[derive(Component)]
+struct SnakeHead;
+
+const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..Default::default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(SnakeHead);
+}
+
 fn main() {
     App::new()
         .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
