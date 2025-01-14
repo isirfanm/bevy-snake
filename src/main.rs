ce44c37c634874a5bac270b6e0525eea59c778bb
Mohamad Irfan Islami <positron06max@gmail.com>
2025-01-14 21:26:55 +0700

    snake movement

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,17 @@ fn spawn_snake(mut commands: Commands) {
         .insert(SnakeHead);
 }
 
+fn snake_movement(mut head_positions: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in head_positions.iter_mut() {
+        transform.translation.y += 2.0;
+    }
+}
+
 fn main() {
     App::new()
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, spawn_snake)
+        .add_systems(Update, snake_movement)
         .add_plugins(DefaultPlugins)
         .run();
 }
