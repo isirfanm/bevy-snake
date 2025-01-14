bedcd291104b3c90dc7c30ba695ada26ba741df7
Mohamad Irfan Islami <positron06max@gmail.com>
2025-01-14 21:05:56 +0700

    default plugin and camera 2d

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Startup, setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
